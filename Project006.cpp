f98c8bf918df394656d7c8de1e870f350c9f79bc
MonLes <73730090+Monles@users.noreply.github.com>
2022-04-03 22:11:09 +0800

    Add files via upload

---
+++ Project006.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project006.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project006, "Project006" );
---
+++ Project006GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Project006GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT006_API AProject006GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
